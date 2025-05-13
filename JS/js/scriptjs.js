e8ed35f4019c6b2a503b6cd49a6233d520ad0dfc
GustavoBuback <80482524+GustavoBuback@users.noreply.github.com>
2025-05-13 20:22:24 -0300

    Add files via upload

---
+++ JS/js/scriptjs.js
@@ -0,0 +1,9 @@
+function enviaDados() 
+{
+    document.getElementById("inputUsuario").innerHTML = "Nome Enviado Foi : " + window.prompt ("Digite Seu Nome:");
+
+
+
+
+
+}
---
+++ JS/script.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+
+document.getElementById("ola_mundo").innerHTML = "Olá Mundo!";
+
+document.getElementById("resposta").innerHTML = "O meu nome é " + window.prompt("Qual é seu nome?");
